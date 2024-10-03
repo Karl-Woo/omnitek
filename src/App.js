4a920f703e1c90c5b8097d415fd73ec977c31552
karlwoo <qw918@nyu.edu>
2024-10-02 23:55:38 -0700

    homepage

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import './styles.css';
 import Header from './components/Header';
+import Description from './components/Description';
 
 function App() {
   return (
     <div className="App">
       <div className='container'>
         <Header></Header>
+        <Description></Description>
       </div>
       <footer className="footer">
         <p className="footer">Footer</p>
---
+++ src/components/Description.js
@@ -0,0 +1,17 @@
+import React from "react";
+import '../styles.css';
+
+export default function Description(){
+    return ( 
+        <div className='description'>
+            <div class="xl-space"></div>
+            <div className='description-text'>
+                <div class="xl-space"></div>
+                <h2>Superior Performance</h2>
+                <div class="m-space"></div>
+                <h3>We ensure excellent phase stability and tightly controlled characteristic impedance, optimizing signal integrity and transmission performance for your systems.</h3>
+                <div class="xl-space"></div>           
+            </div>  
+        </div>
+    );
+}
