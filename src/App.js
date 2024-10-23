b51b4358523df03cf02a872d897231c288999def
karlwoo <qw918@nyu.edu>
2024-10-22 21:57:44 -0700

    test

--- src/App.js
+++ src/App.js
@@ -1,57 +1,71 @@
-import './App.css';
-import './styles.css';
-import Home from './components/Home';
-import Aboutus from './components/Aboutus';
-import Products from './components/Products';
-import Applications from './components/Applications';
-import Contactus from './components/Contactus';
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import "./App.css";
+import "./styles.css";
+import Home from "./components/Home";
+import Aboutus from "./components/Aboutus";
+import Products from "./components/Products";
+import Applications from "./components/Applications";
+import Contactus from "./components/Contactus";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      
-        <Router>
-          <nav className="navbar">
-            <div className="navbar-left">
-              <a href="/" className="logo">
-              <img className="homeimg" src={`./omnitek/images/home.png`} alt="home" />
-              </a>
-            </div>
+      <Router>
+        <nav className="navbar">
+          <div className="navbar-left">
+            <a href="/" className="logo">
+              <img
+                className="homeimg"
+                src={`./omnitek/images/home.png`}
+                alt="home"
+              />
+            </a>
+          </div>
 
-            <div className="navbar-right">
-              <ul>
-                <li>
-                  <a className="textlink" href="/">Home</a>
-                </li>
-                <li>
-                  <a className="textlink" href="/aboutus">About us</a>
-                </li>
-                <li>
-                  <a className="textlink" href="/products">Products</a>
-                </li>
-                <li>
-                  <a className="textlink" href="/applications">Applications</a>
-                </li>
-                <li>
-                  <a className="textlink" href="/products">Search</a>
-                </li>
-              </ul>
-              <a href="/contactus"><button className="primary-button">Contact Us</button></a>
-            </div>
-            
-          </nav>
-          <div className='container'>
+          <div className="navbar-right">
+            <ul>
+              <li>
+                <a className="textlink" href="/omnitek">
+                  Home
+                </a>
+              </li>
+              <li>
+                <a className="textlink" href="/aboutus">
+                  About us
+                </a>
+              </li>
+              <li>
+                <a className="textlink" href="/products">
+                  Products
+                </a>
+              </li>
+              <li>
+                <a className="textlink" href="/applications">
+                  Applications
+                </a>
+              </li>
+              <li>
+                <a className="textlink" href="/products">
+                  Search
+                </a>
+              </li>
+            </ul>
+            <a href="/contactus">
+              <button className="primary-button">Contact Us</button>
+            </a>
+          </div>
+        </nav>
+        <div className="container">
           <Routes>
-            <Route path="/" element={<Home />}></Route>
+            <Route path="/omnitek" element={<Home />}></Route>
             <Route path="/aboutus" element={<Aboutus />}></Route>
             <Route path="/products" element={<Products />}></Route>
             <Route path="/applications" element={<Applications />}></Route>
             <Route path="/contactus" element={<Contactus />}></Route>
           </Routes>
-          </div>
-        </Router>
-      
+        </div>
+      </Router>
+
       <footer className="footer">
         <p className="footer">Footer</p>
       </footer>
--- src/components/Products.js
+++ src/components/Products.js
@@ -1,44 +1,102 @@
-import React, {useState, useEffect} from 'react';
-import '../styles.css';
+import React, { useState, useEffect } from "react";
+import "../styles.css";
 
-export default function Aboutus(){
+export default function Aboutus() {
+  const [productitem, setProductitem] = useState([]);
 
-    const [productitem, setProductitem] = useState([]);
+  useEffect(() => {
+    fetch("./omnitek/products.json")
+      .then((response) => response.json())
+      .then((data) => setProductitem(data));
+  }, []);
 
-    useEffect(() => {
-
-        fetch("./omnitek/product.json")
-        .then(response => response.json())
-        .then(data => setProductitem(data));
-        
-    },[]);
-
-    return ( 
-        <div className='product-sec'>
-            <div className="xl-space"></div>
-            <div className='product-text'>
-                <h2>Products</h2>
-                <div className="l-space"></div>
-                <div className='product-content'>
-                    {
-                        productitem.map(item => (
-                            <div key={item.id} className="product-item">
-                                <img src={`./omnitek/images/${item.image}`} alt={item.title}/>
-                                <div className="l-space"></div>
-                                <div>
-                                    <s1 className="body">{item.title}</s1>
-                                    <div className="m-space"></div>
-                                    <p className="body">{item.content}</p>
-                                    <div className="m-space"></div>
-                                    <i className="right-icon"></i>
-                                    <div className="s-space"></div>
-                                </div>        
-                            </div>
-                        ))
-                    }
-                </div>                      
-            </div>  
-            <div className="xl-space"></div>
+  return (
+    <div className="products">
+      <div className="xl-space"></div>
+      <div className="solution-text">
+        <h1>Products</h1>
+        <div className="l-space"></div>
+        <input className="searchbar" type="text" placeholder="Search here" />
+        <div className="l-space"></div>
+        <hr />
+        <div className="l-space"></div>
+        <h2>Coax Cable</h2>
+        <div className="m-space"></div>
+        <p>
+          Our RF cables provide reliable signal transmission across a wide
+          frequency range. Ideal for telecommunications, broadcasting
+          applications where performance is critical.
+        </p>
+        <div className="product-content">
+          {productitem.map((item) => (
+            <div key={item.id} className="products-item">
+              <img src={`./omnitek/images/${item.image}`} alt={item.title} />
+              <div className="l-space"></div>
+              <div>
+                <s1 className="body">{item.title}</s1>
+                <div className="m-space"></div>
+                <p className="body">{item.content}</p>
+                <div className="m-space"></div>
+                <i className="right-icon"></i>
+                <div className="s-space"></div>
+              </div>
+            </div>
+          ))}
         </div>
-    );
+        <div className="l-space"></div>
+        <hr />
+        <div className="l-space"></div>
+        <h2>High Speed Twin-ax Cable</h2>
+        <div className="m-space"></div>
+        <p>
+          Our RF cables provide reliable signal transmission across a wide
+          frequency range. Ideal for telecommunications, broadcasting
+          applications where performance is critical.
+        </p>
+        <div className="product-content">
+          {productitem.map((item) => (
+            <div key={item.id} className="products-item">
+              <img src={`./omnitek/images/${item.image}`} alt={item.title} />
+              <div className="l-space"></div>
+              <div>
+                <s1 className="body">{item.title}</s1>
+                <div className="m-space"></div>
+                <p className="body">{item.content}</p>
+                <div className="m-space"></div>
+                <i className="right-icon"></i>
+                <div className="s-space"></div>
+              </div>
+            </div>
+          ))}
+        </div>
+        <div className="l-space"></div>
+        <hr />
+        <div className="l-space"></div>
+        <h2>Other Cables</h2>
+        <div className="m-space"></div>
+        <p>
+          Our RF cables provide reliable signal transmission across a wide
+          frequency range. Ideal for telecommunications, broadcasting
+          applications where performance is critical.
+        </p>
+        <div className="product-content">
+          {productitem.map((item) => (
+            <div key={item.id} className="products-item">
+              <img src={`./omnitek/images/${item.image}`} alt={item.title} />
+              <div className="l-space"></div>
+              <div>
+                <s1 className="body">{item.title}</s1>
+                <div className="m-space"></div>
+                <p className="body">{item.content}</p>
+                <div className="m-space"></div>
+                <i className="right-icon"></i>
+                <div className="s-space"></div>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+      <div className="xl-space"></div>
+    </div>
+  );
 }
