e5df13e1cfc32b566d7660468c8757d80aa35239
karlwoo <qw918@nyu.edu>
2025-01-26 22:01:20 -0800

    fix scroll

--- src/components/Products.js
+++ src/components/Products.js
@@ -13,14 +13,39 @@ export default function Products() {
     fetch("./products.json")
       .then((response) => response.json())
       .then((data) => setProductitem(data));
+  }, []);
 
+  useEffect(() => {
     // Get the search term from the URL
     const params = new URLSearchParams(location.search);
     const term = params.get("search");
     if (term) {
       setSearchTerm(term);
     }
-  }, [location.search]);
+
+    if (location.hash) {
+      const sectionId = location.hash.replace("#", "");
+      const scrollToSection = () => {
+        const section = document.getElementById(sectionId);
+        if (section) {
+          // Add an offset to the scroll position
+          const offset = 100; // Adjust this value for your desired offset
+
+          // Calculate the position to scroll to with the offset
+          const sectionPosition = section.offsetTop - offset;
+
+          // Scroll to the section with the offset
+          window.scrollTo({
+            top: sectionPosition,
+            behavior: "smooth",
+          });
+        }
+      };
+
+      // Add a small delay to ensure the section is rendered
+      setTimeout(scrollToSection, 100);
+    }
+  }, [location]);
 
   const handleSearchChange = (e) => {
     setSearchTerm(e.target.value);
@@ -37,6 +62,7 @@ export default function Products() {
   const filteredHigh = filteredproducts.filter(
     (item) => item.type === "Data Cable"
   );
+
   const filteredOther = filteredproducts.filter(
     (item) => item.type === "Customized Cable"
   );
@@ -56,71 +82,76 @@ export default function Products() {
         />
         <div className="l-space"></div>
         <hr />
-
-        {filteredCoax.length > 0 && (
-          <div className="products-content" id="1">
-            <div className="l-space"></div>
-            <h2 className="dis solution-text">RF Cable</h2>
-            <p className="dis solution-text">
-              High-frequency cables designed for reliable signal transmission in
-              demanding RF and microwave applications.
-            </p>
+        <div id="1">
+          {filteredCoax.length > 0 && (
             <div className="products-content">
-              {filteredCoax.map((item) => (
-                <div
-                  key={item.id}
-                  className="products-item"
-                  onClick={() => navigate(`${item.id}`)}
-                >
-                  <img src={`./images/${item.image}`} alt={item.title} />
-                  <div className="l-space"></div>
-                  <div>
-                    <b className="body">{item.title}</b>
-                    <div className="m-space"></div>
-                    <p className="body">{item.content}</p>
-                    <div className="m-space"></div>
-                    <i className="right-icon"></i>
-                    <div className="s-space"></div>
+              <div className="l-space"></div>
+              <h2 className="dis solution-text">RF Cable</h2>
+              <p className="dis solution-text">
+                High-frequency cables designed for reliable signal transmission
+                in demanding RF and microwave applications.
+              </p>
+              <div className="products-content">
+                {filteredCoax.map((item) => (
+                  <div
+                    key={item.id}
+                    className="products-item"
+                    onClick={() => navigate(`${item.id}`)}
+                  >
+                    <img src={`./images/${item.image}`} alt={item.title} />
+                    <div className="l-space"></div>
+                    <div>
+                      <b className="body">{item.title}</b>
+                      <div className="m-space"></div>
+                      <p className="body">{item.content}</p>
+                      <div className="m-space"></div>
+                      <i className="right-icon"></i>
+                      <div className="s-space"></div>
+                    </div>
                   </div>
-                </div>
-              ))}
+                ))}
+              </div>
+              <div className="l-space"></div>
             </div>
-            <div className="l-space"></div>
-          </div>
-        )}
-        {filteredHigh.length > 0 && (
-          <div className="products-content" id="2">
-            <h2 className="dis solution-text">Data Cable</h2>
-            <p className="dis solution-text">
-              High-performance cables for fast, secure data transmission across
-              various digital and multimedia applications.
-            </p>
+          )}
+        </div>
+        <div id="2">
+          {filteredHigh.length > 0 && (
             <div className="products-content">
-              {filteredHigh.map((item) => (
-                <div
-                  key={item.id}
-                  className="products-item"
-                  onClick={() => navigate(`${item.id}`)}
-                >
-                  <img src={`./images/${item.image}`} alt={item.title} />
-                  <div className="l-space"></div>
-                  <div>
-                    <b className="body">{item.title}</b>
-                    <div className="m-space"></div>
-                    <p className="body">{item.content}</p>
-                    <div className="m-space"></div>
-                    <i className="right-icon"></i>
-                    <div className="s-space"></div>
+              <h2 className="dis solution-text">Data Cable</h2>
+              <p className="dis solution-text">
+                High-performance cables for fast, secure data transmission
+                across various digital and multimedia applications.
+              </p>
+              <div className="products-content">
+                {filteredHigh.map((item) => (
+                  <div
+                    key={item.id}
+                    className="products-item"
+                    onClick={() => navigate(`${item.id}`)}
+                  >
+                    <img src={`./images/${item.image}`} alt={item.title} />
+                    <div className="l-space"></div>
+                    <div>
+                      <b className="body">{item.title}</b>
+                      <div className="m-space"></div>
+                      <p className="body">{item.content}</p>
+                      <div className="m-space"></div>
+                      <i className="right-icon"></i>
+                      <div className="s-space"></div>
+                    </div>
                   </div>
-                </div>
-              ))}
+                ))}
+              </div>
+              <div className="l-space"></div>
             </div>
-            <div className="l-space"></div>
-          </div>
-        )}
+          )}
+        </div>
         {filteredOther.length > 0 && (
-          <div className="products-content" id="3">
-            <h2 className="dis solution-text">Customized Cables</h2>
+          <div className="products-content">
+            <h2 className="dis solution-text" id="3">
+              Customized Cables
+            </h2>
             <p className="dis solution-text">
               Specialized cables engineered to match unique specifications for
               specific industry applications.
@@ -153,13 +184,6 @@ export default function Products() {
             </div>
           </div>
         )}
-        {filteredOther.length === 0 &&
-          filteredHigh.length === 0 &&
-          filteredCoax.length === 0 && (
-            <div className="products-content">
-              <h2 className="dis">No Product Found</h2>
-            </div>
-          )}
       </div>
       <div className="xl-space"></div>
     </div>
